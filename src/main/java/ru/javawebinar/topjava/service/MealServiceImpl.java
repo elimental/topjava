6b4a3231c439d4dd55b35f2c3bec69ee0a3a08f3
Dima <andreevdb@gmail.com>
2019-04-25 14:44:41 +0300

    getMealWithUser and test added

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,14 +19,24 @@ public class MealServiceImpl implements MealService {
 
     private final MealRepository repository;
 
+    private final UserRepository userRepository;
+
     @Autowired
-    public MealServiceImpl(MealRepository repository) {
+    public MealServiceImpl(MealRepository repository, UserRepository userRepository) {
         this.repository = repository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public Meal get(int id, int userId) {
         return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    @Override
+    public Meal getWithUser(int id, int userId) throws NotFoundException {
+        Meal meal = repository.get(id, userId);
+        meal.setUser(userRepository.get(userId));
+        return meal;
     }
 
     @Override
--- src/test/java/ru/javawebinar/topjava/service/DataJPAMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/DataJPAMealServiceTest.java
@@ -1,7 +1,23 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 
 @ActiveProfiles({"postgres", "datajpa"})
 public class DataJPAMealServiceTest extends AbstractMealServiceTest {
+
+    @Test
+    public void getWithUser() {
+        Meal actualMeal = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
+        User actualUser = actualMeal.getUser();
+        assertMatch(actualMeal, ADMIN_MEAL1);
+        UserTestData.assertMatch(actualUser, ADMIN);
+    }
 }
