2afff527dd6a8853c564c6ffa9ed81366b339207
Dima <elimental@bk.ru>
2019-05-28 22:47:28 +0300

    HW07 MealRestController hardcoded, date and time formatters added

---
+++ src/main/java/ru/javawebinar/topjava/util/SpringMVCDateFormatter.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.util;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class SpringMVCDateFormatter implements Formatter<LocalDate> {
+
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        return LocalDate.parse(text);
+    }
+
+    @Override
+    public String print(LocalDate object, Locale locale) {
+        return null;
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealRestControllerTest extends AbstractControllerTest {
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getBetween() {
+    }
+}
