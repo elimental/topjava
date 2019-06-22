59096854b51a6275c5292512672257c944dbe49b
Dima <elimental@bk.ru>
2019-06-22 17:38:17 +0300

    HW08 without optional have done

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/profile/meals")
+public class MealAjaxController extends AbstractMealController {
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void createOrUpdate(@RequestParam("dateTime") LocalDateTime dateTime,
+                               @RequestParam("description") String description,
+                               @RequestParam("calories") int calories) {
+
+        Meal meal = new Meal(dateTime, description, calories);
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+    }
+}
